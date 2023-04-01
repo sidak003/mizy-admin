481a60d99dfa021ff3c8b18725dfac14572e915d
sidak003 <sidakdhingra100@gmail.com>
2023-04-01 20:14:22 +0530

    beta-fix

--- app/AuthManager.js
+++ app/AuthManager.js
@@ -1,10 +1,28 @@
 'use client'
-import { Amplify } from 'aws-amplify';
 import { Authenticator } from '@aws-amplify/ui-react';
+import { Amplify } from 'aws-amplify';
 import '@aws-amplify/ui-react/styles.css';
-import awsExports from '../src/aws-exports';
-Amplify.configure(awsExports);
 
+
+Amplify.configure({
+        aws_project_region: process.env.awsRegion,
+        aws_cognito_identity_pool_id: process.env.cognitoIdentityPoolId,
+        aws_cognito_region: process.env.awsRegion,
+        aws_user_pools_id: process.env.awsUserPoolId,
+        aws_user_pools_web_client_id: process.env.awsUserPoolWebClientId,
+        oauth: {},
+        aws_cognito_username_attributes: [],
+        aws_cognito_social_providers: [],
+        aws_cognito_signup_attributes: ["EMAIL"],
+        aws_cognito_mfa_configuration: "OFF",
+        aws_cognito_mfa_types: ["SMS"],
+        aws_cognito_password_protection_settings: {
+            passwordPolicyMinLength: 8,
+            passwordPolicyCharacters: []
+        },
+        aws_cognito_verification_mechanisms: ["EMAIL"],
+    }
+);
 
 const formFields = {
     confirmVerifyUser: {
--- next.config.js
+++ next.config.js
@@ -5,6 +5,10 @@ const nextConfig = {
   },
   env : {
     adminApii : 'https://mhtbnkfa67.execute-api.ap-south-1.amazonaws.com/dev',
+    awsRegion : "ap-south-1",
+    cognitoIdentityPoolId : "ap-south-1:5c1796b9-ea3b-4bd6-92a6-74ab7e4f425a",
+    awsUserPoolId : "ap-south-1_wAx4BKiqz",
+    awsUserPoolWebClientId : "4orbp7hqlirnlg1qp0io4aa3kc",
   },
 }
 
